78d42a8a2646b67ca1c58923aec7d018bbc9d4c5
Jihopark007 <jihopark007@gmail.com>
2024-07-21 16:49:55 +0900

    Add files via upload

---
+++ 4_naming_conventions.js
@@ -0,0 +1,31 @@
+/**
+ * Naming Convetions
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ *    1name, 2hello
+ * 4) 키워드는 변수명으로 사용 할 수 없다.
+ *    var const = 'var';
+ */
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yuJin);
+
+// let 1name = 'no';
+// let const = 'attention';
+
+/**
+ * Naming Convention 2
+ * 
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ *    띄어쓰기 하고 난 다음 첫번째 글자를 대문자로 사용
+ * 2) snake_case -> 파이썬같은 언어에서 사용
+ * 3) PascalCase -> c#같은 마이크로소프트 계열의 언어에서 사용한다
+ */
+const anYuJin = '안유진';
+console.log(anYuJin);
